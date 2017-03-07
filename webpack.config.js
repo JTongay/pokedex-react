7a08d9a5f7521e7c65f7483f5d0e073b602dfcb3
Joseph Tongay <dirk.tongay@gmail.com>
2017-03-06 20:11:42 -0600

    start thinking of ideas

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = {
 
@@ -9,7 +10,7 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: '/app/'
     },
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [new HtmlWebpackPlugin(), new DashboardPlugin()],
     module: {
         loaders: [
             {
