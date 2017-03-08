ce36071cfefc4ad0f92daabb045ba3418dd4cafa
Joseph Tongay <dirk.tongay@gmail.com>
2017-03-07 21:11:35 -0600

    repeated pokeballs by number of hard coded generations and added sass compiling

--- src/app/js/components/home.component.js
+++ src/app/js/components/home.component.js
@@ -1,29 +1,41 @@
 import React, { PropTypes } from 'react'
 import axios from 'axios'
 
+require('../../scss/home.scss')
+
 const HomeComponent = React.createClass({
 
   getInitialState(){
     return {
-      pokemon: []
+      pokemon: [],
+      gens: [1,2,3,4,5,6]
     }
   },
 
   componentWillMount(){
-    axios.get('http://pokeapi.co/api/v2/pokemon/4').then((res)=>{
+    axios.get("http://pokeapi.co/api/v2/pokemon/650/").then((res)=>{
       console.log(res.data);
-      this.setState({
-        pokemon: res.data
-      })
     })
   },
 
+  //helper functions
+
+
   render () {
+    //Repeat image to total number of generations
+    let numOfGens = this.state.gens;
+    numOfGens = numOfGens.map((gen, index)=>{
+      return (
+        <li key={gen.toString()}><img src="http://vignette3.wikia.nocookie.net/youtubepoop/images/4/4c/Pokeball.png/revision/latest?cb=20150418234807" /></li>
+      )
+    })
+
     return (
       <div>
         <h1>Welcome to the React Pokedex! This was all made in React</h1>
-        <img src="http://vignette3.wikia.nocookie.net/youtubepoop/images/4/4c/Pokeball.png/revision/latest?cb=20150418234807" />
-        <p>{this.state.pokemon.name}</p>
+        <ul>
+          {numOfGens}
+        </ul>
       </div>
     )
   }
