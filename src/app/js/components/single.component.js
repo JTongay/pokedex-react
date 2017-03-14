274f8d243bf475c029b2b84bac12b2bd1b9b5fb0
Joseph Tongay <dirk.tongay@gmail.com>
2017-03-13 19:39:49 -0500

    testing looping through images, but its only spitting out one. wtf

--- src/app/js/components/single.component.js
+++ src/app/js/components/single.component.js
@@ -1,42 +1,36 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 import axios from 'axios'
 
 const SingleGenComponent = React.createClass({
-  getInitialState(){
-    console.log(this.props);
-    return {
-      chosenGen: this.props.chosenGen,
-      generationNumber: null,
-      totalPokemon: [],
-      region: null,
+    getInitialState() {
+        console.log(this.props);
+        return {
+          chosenGen: this.props.chosenGen,
+          generationNumber: null,
+          totalPokemon: [],
+          region: null
+        }
+    },
+
+    pokeImgLoop(){
+      for(var i = 1; i < 10; i++){
+        return (
+          <div>
+            <img src={`https://veekun.com/dex/media/pokemon/global-link/${i}.png`}/>
+          </div>
+        )
+      }
+    },
+
+    render() {
+        return (
+          <div>
+            <h1>Booyah</h1>
+            <h2>Gen {this.props.chosenGen.id}</h2>
+            {this.pokeImgLoop()}
+          </div>
+        )
     }
-  },
-
-  componentWillMount(){
-    console.log(this.state.chosenGen);
-    // for(var i = this.state.chosenGen.genStart; i <= this.state.chosenGen.genEnd; i++){
-    //   axios.get("http://pokeapi.co/api/v2/pokemon/" + i).then((res)=>{
-    //     console.log(res.data);
-    //     this.state.totalPokemon.push(res.data)
-    //   })
-    // }
-  },
-
-  render () {
-    console.log(this.state.chosenGen);
-    console.log(this.state.totalPokemon);
-    // let generation = this.state.totalPokemon;
-    // generation = generation.map((poke, index)=>{
-    //   return (
-    //     <li>{poke.name}</li>
-    //   )
-    // })
-    return (
-    <div>
-      <h1>Gen {this.props.genNum}</h1>
-    </div>
-    )
-  }
 })
 
 export default SingleGenComponent
