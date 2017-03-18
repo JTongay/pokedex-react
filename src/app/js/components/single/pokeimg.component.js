8bc1b33a9cac9ed4338dddf7429cde914c1aecfc
Joseph Tongay <dirk.tongay@gmail.com>
2017-03-17 19:42:39 -0500

    ok seriously, do some more tutorials. thinking in components makes no sense

--- src/app/js/components/single/pokeimg.component.js
+++ src/app/js/components/single/pokeimg.component.js
@@ -6,15 +6,14 @@ export default class PokeImgComponent extends React.Component {
   }
 
   componentWillMount(){
-    let pokeNumber = parseInt(this.props.id)
-    console.log(pokeNumber);
-    this.setState({
-      allPokemon: this.props.showGenPokemon(this.props.id)
-    })
+    // let pokeNumber = parseInt(this.props.id)
+    // console.log(pokeNumber);
+    // this.setState({
+    //   allPokemon: this.props.showGenPokemon(this.props.id)
+    // })
   }
 
   render () {
-    console.log(this.state.allPokemon);
     return (
       <img src={`https://veekun.com/dex/media/pokemon/global-link/${this.props.id}.png`} />
     )
